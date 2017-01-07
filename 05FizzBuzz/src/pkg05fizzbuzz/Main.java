49e3a8f63179c9176220721234deefac52d86ee8
nemezisso <b.kochanowski11@gmail.com>
2017-01-07 15:19:20 +0100

    Fix

--- 05FizzBuzz/src/pkg05fizzbuzz/Main.java
+++ 05FizzBuzz/src/pkg05fizzbuzz/Main.java
@@ -6,12 +6,12 @@ public class Main {
         int tab[]=new int[100];
         for(int i=0;i<100;i++){
             tab[i]=i+1;
-            if(tab[i]%3==0)
+            if(tab[i]%15==0)
+                System.out.println("FIZZBUZZ");
+            else if(tab[i]%3==0)
                 System.out.println("FIZZ");
             else if(tab[i]%5==0)
                 System.out.println("BUZZ");
-            else if(tab[i]%15==0)
-                System.out.println("FIZZBUZZ");
             else
                 System.out.println(tab[i]);
         }
